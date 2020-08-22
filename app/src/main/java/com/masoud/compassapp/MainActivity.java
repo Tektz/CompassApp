02159e17d40d50544ad5c3c646e06b2fc5003f17
stechdit <34957383+stechdit@users.noreply.github.com>
2020-08-22 17:16:35 +0300

    initial

---
+++ app/src/main/java/com/masoud/compassapp/MainActivity.java
@@ -0,0 +1,55 @@
+package com.masoud.compassapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+   private ImageView image;
+   private float currentDegree = 0f;
+   private SensorManager mSensorManager;
+   TextView tvHeading;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        tvHeading = (TextView) findViewById(R.id.tvHeading);
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mSensorManager.registerListener((SensorEventListener) this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_GAME);
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener((SensorEventListener) this);
+    }
+
+    public void onSensorChanged(SensorEvent event){
+        float degree = Math.round(event.values[0]);
+        tvHeading.setText("Heading: " + degree + " degrees");
+        RotateAnimation ra = new RotateAnimation(
+                currentDegree, -degree,
+                Animation.RELATIVE_TO_SELF, 0.5f,
+                Animation.RELATIVE_TO_SELF, 0.5f);
+        ra.setDuration(210);
+        ra.setFillAfter(true);
+        image.startAnimation(ra);
+        currentDegree = -degree;
+    }
+
+
+}
